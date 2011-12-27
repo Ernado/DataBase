b3c4d88d45e2af5f2468e26a147af21d9acb69e8
Ernado <sertav.ernado@gmail.com>
2011-12-27 23:51:37 +0400

    added error handling for some functions

--- database.pas
+++ database.pas
@@ -1,19 +1,18 @@
 program PDataBase;
 uses
-  crt{, repository};
+  crt{, repository}{, .. };
 
 const
   DBFPATH = 'database.db';
 
-
-
-type user = record
+type Tuser = record
   name, surname, school, city, dob: string;
 end;
 
 type TDataBase = object
      dataFile:text;
      count:byte;
+     session:boolean;
      private
      _iterator:word;
      public
@@ -21,6 +20,7 @@ type TDataBase = object
      procedure openSession;
      procedure closeSession;
      function iterator:word;
+     function getUser:Tuser;
      private
      function checkFormat:boolean;
      procedure loadParams;
@@ -31,17 +31,21 @@ end;
 var
   dataBase:TDataBase;
 
-{выводит на экран сообщение об ошибке и завершает программу с кодом }
-procedure errorHandle(msg:string; stop:boolean=true);
+{выводит на экран сообщение об ошибке и завершает программу с кодом
+         msg - сообщение
+         stop - завершене программы после отображения ошибки}
+
+procedure raiseError(msg:string; stop:boolean=true);
 begin
      ClrScr;
-     WriteLn('ERROR OCCURED');
+     WriteLn('RUNTIME ERROR');
      Writeln(msg);
      ReadKey;
      if stop then halt(-1);
 end;
 
-{конвертирует строку в число с обработкой ошибок}
+{конвертирует строку в число с обработкой ошибок
+              s - строка, содержащая число}
 function value(s:string):integer;
 var
   _code:integer;
@@ -51,16 +55,17 @@ begin
      if _code <> 0 then
      begin
         _t := 'CONVERTING ERROR on converting <' + s + '> to integer';
-        errorHandle(_t);
+        raiseError(_t);
      end;
 end;
 
-{иницирует базу данных с обработкой ошибок}
+{иницирует базу данных с обработкой ошибок
+           path - путь к базе данных}
 Constructor TdataBase.init(path:string);
 begin
      {$I-}Assign(dataFile,path);
-     if (IOResult <> 0) then errorHandle('IO ERROR on database read');   {$I+}
-     if not checkFormat then errorHandle('DATABASE FORMAT ERROR on database read');
+     if (IOResult <> 0) then raiseError('IO ERROR on database read');   {$I+}
+     if not checkFormat then raiseError('DATABASE FORMAT ERROR on database read');
      loadParams;
 end;
 
@@ -78,13 +83,23 @@ end;
 {открывает файл на чтение}
 procedure TdataBase.openSession;
 begin
+     if session then raiseError('SESSION ALREADY OPENED on session start');
+     {$I-}
      Reset(dataFile);
+     if (IOResult <> 0) then raiseError('IO ERROR on session start');
+     {$+}
+
+     session := true;
 end;
 
-{закрывает файл}
+{закрывает сессию}
 procedure TdataBase.closeSession;
 begin
+     if not session then raiseError('NO SESSION TO CLOSE on session close');
+     {$I-}
      Close(dataFile);
+     if (IOResult <> 0) then raiseError('IO ERROR on session close');
+     {$I+}
 end;
 
 {загружает информацию о базе данных}
@@ -106,9 +121,10 @@ end;
 
 procedure TdataBase.skipLine;
 begin
+     if no session then raiseError('NO SESSION ERROR on skip line');
      {$I-}
      ReadLn(dataFile);
-     if (IOResult <> 0) then errorHandle('IO ERROR on skip line');
+     if (IOResult <> 0) then raiseError('IO ERROR on skip line');
      {$I+}
 end;
 
@@ -119,6 +135,25 @@ var
 begin
      ReadLn(dataFile,_t);
      getLine := _t;
+end;
+
+{вовзращает сущность из базы данных}
+function TdataBase.getUser:TUser;
+begin
+     if not session then raiseError('NO SESSION ERROR on getUser');
+     {$I-}
+     if (getLine <> 'user') then raiseError('TYPE ERROR on getUser');
+     with getUser do
+     begin
+          name:=getLine;
+          surname:=getLine;
+          dob:=getLine;
+          school:=getLine;
+          city:=getLine;
+     end;
+     if (getLine <> 'enduser') then raiseError('TYPE ERROR on getUser');
+     if (IOResult <> 0) then  raiseError('IO ERROR on getUser');
+     {$I+}
 end;
 
 begin
