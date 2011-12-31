93520e2be0c076e913a2b6d668c064d64cfdc792
Alex <sertav.ernado@gmail.com>
2011-12-31 19:03:53 +0400

    refactored main programm

--- database.pas
+++ database.pas
@@ -1,41 +1,11 @@
-{DataBase v0.1.7 by Razumov}
+{DataBase v1.0 by Razumov}
 program PDataBase;
 uses
-    crt, UDB, errors, menus, lists;
-
-var
-  dataBase:TDataBase;
-  user:TUser;
+    errors, menus;
 
 begin
-{ChDir('D:\Work\liceum\database');}
 context.count:=0;
-context.Deep('Main');   {
-dataBase.init(DBFPATH);
-dataBase.skipToData;
-with user do
-begin
-  name:='Имя';
-  surname:='Фамилия';
-  dob:='Дата рожд.';
-  city:='Город';
-  school:='Учебное заведение';
-end;
-user.Print;
-with user do
-begin
-  name:='===========';
-  surname:='===========';
-  dob:='==========';
-  city:='===========';
-  school:='====================';
-end;
-user.Print;
-repeat
-      database.getUser(user);
-      user.Print;
-until eof(dataBase.dataFile);
-ReadKey;}
+context.Deep('Main');
 MainMenu;
 context.Up;
 end.
